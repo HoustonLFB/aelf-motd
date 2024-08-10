8d348f123a1719b2756c6984aa44ef987091ce00
HoustonLFB <23219631+HoustonLFB@users.noreply.github.com>
2024-08-10 17:54:14 +0200

    template

---
+++ src/client/java/houstonlfb/aelfmotd/AELFMOTDClient.java
@@ -0,0 +1,10 @@
+package houstonlfb.aelfmotd;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AELFMOTDClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
