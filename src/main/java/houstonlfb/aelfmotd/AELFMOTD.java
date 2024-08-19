8095a77e4cc24530f0650707361be7a8e64089fd
Houston <23219631+HoustonLFB@users.noreply.github.com>
2024-08-19 10:11:58 +0200

    add import

--- src/main/java/houstonlfb/aelfmotd/AELFMOTD.java
+++ src/main/java/houstonlfb/aelfmotd/AELFMOTD.java
@@ -6,13 +6,13 @@ import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDate;
+
+import java.io.*;
+import java.util.Properties;
 
 public class AELFMOTD implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
