86473a91d2836d45f86e14eeed46d027c77fb750
HoustonLFB <23219631+HoustonLFB@users.noreply.github.com>
2024-08-22 20:06:05 +0200

    1.3.0 Commits AelfMC

--- src/main/java/houstonlfb/aelfmotd/AelfMC.java
+++ src/main/java/houstonlfb/aelfmotd/AelfMC.java
@@ -6,7 +6,7 @@ import net.fabricmc.api.DedicatedServerModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Aelf implements DedicatedServerModInitializer {
+public class AelfMC implements DedicatedServerModInitializer {
     // This logger is used to write text to the console and the log file.
     // It is considered best practice to use your mod id as the logger's name.
     // That way, it's clear which mod wrote info, warnings, and errors.
@@ -19,8 +19,7 @@ public class Aelf implements DedicatedServerModInitializer {
         LOGGER.info("AELF MOTD starting...");
 
         CommandClass.aelfCommand();
-		Motd.motd();
-
+        Motd.motd();
 
 
     }
--- src/main/java/houstonlfb/aelfmotd/Motd.java
+++ src/main/java/houstonlfb/aelfmotd/Motd.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
+
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
@@ -11,7 +12,6 @@ import java.time.LocalDate;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
 
 
 public class Motd {
